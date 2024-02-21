b2731f72ae782e0bd95cd51cde0510d04c1f4bd9
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-02-21 16:06:25 +0300

    ft: add script to back up negpod_10-q1 to a remote computer

--- backup-Negpod_10.sh
+++ backup-Negpod_10.sh
@@ -5,14 +5,12 @@ host="64293e56bc62.3a2627c1.alu-cod.online"
 username="64293e56bc62"
 password="328d3b338a4ced526c9a"
 
-local_dir="/home/mi/Documents/workdir/ALU/summatives/anonymous_test"
+local_dir="/home/mi/Documents/workdir/ALU/summative-2_GSC10/negpod_10-q1"
 remote_dir="/summative/1023-2024j"
-files_to_backup="file1.txt file2.txt file3.txt"
-for file in $files_to_backup; do
-    scp "$local_dir/$file" "$username@$host:$remote_dir"
-    if [ $? -eq 0 ]; then
-        echo "Backup of $file successful"
-    else
-        echo "Backup of $file failed"
-    fi
-done
+
+scp -r "$local_dir" "$username@$host:$remote_dir"
+if [ $? -eq 0 ]; then
+    echo "Backup successful"
+else
+    echo "Backup failed"
+fi
