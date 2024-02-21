3a58b52b2cf4a2c49ce3eaae002d3942c9776f7b
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-02-21 15:59:52 +0300

    fix: unnamed directory

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -5,6 +5,6 @@ mkdir -p negpod_10-q1
 
 # Move the files to the directory
 mv main.sh negpod_10-q1/
-mv Students-list_1023.txt nnegpod_10-q1/
+mv Students-list_1023.txt negpod_10-q1/
 mv Select-emails.sh negpod_10-q1/
 mv student-emails.txt negpod_10-q1/
